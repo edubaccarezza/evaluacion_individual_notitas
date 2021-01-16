8c18f81f53205d7f7c32c88b822aeb92c5de19b2
edubaccarezza <edubaccarezza@gmail.com>
2021-01-16 13:54:52 -0300

    Trabajo finalizado

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -20,5 +20,49 @@ module.exports = {
                 unaNota: unaNota
             })
         })
-    }    
+    },
+    edit: function(req, res) {
+        db.Nota.update({
+            title: req.body.title,
+            description: req.body.description,
+        }, {
+            where: {
+                id: req.params.id
+            } 
+        })
+        .then(function(notaEditada){
+            res.redirect('/')
+        })
+    },
+    save: function(req,res) {
+        // res.send(req.body)
+        db.Nota.create({
+            title: req.body.title,
+            description:req.body.description,
+        })
+        .then(function(notaCreada) {
+            
+            res.redirect('../')
+        })
+    },
+    // toDestroy: function(req, res){
+    //     db.Nota.destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     })
+    //     .then(function(){
+    //         res.redirect('/')
+    //     })
+    // },
+    delete: function(req, res) {
+        db.Nota.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function(){
+                res.redirect('/')
+        })
+    }
 }
--- src/routes/note.js
+++ src/routes/note.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 const noteController = require ("../controllers/noteController");
 
+
+// CREAR
 router.get('/', noteController.index);
+router.post('/', noteController.save);
+
+//EDITAR
 router.get('/detail/:id', noteController.detail);
+router.post('/detail/:id', noteController.edit);
+
+//BORRAR
+router.delete('/:id', noteController.delete)
+// router.post('/:id', noteController.toDestroy);
+
 
 module.exports = router;
